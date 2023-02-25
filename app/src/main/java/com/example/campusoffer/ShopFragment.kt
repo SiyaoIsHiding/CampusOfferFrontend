fc278a02ffb59d9ffbe7ebfd47a9a8b9928629c8
Fengnan <soniasun0925@gmail.com>
2023-02-25 00:58:51 -0800

    "Set up Navigation Bar, RecyclerView"

--- app/src/main/java/com/example/campusoffer/ShopFragment.kt
+++ app/src/main/java/com/example/campusoffer/ShopFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.todkars.shimmer.ShimmerRecyclerView
 
 class ShopFragment : Fragment() {
 
@@ -14,7 +15,8 @@ class ShopFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_shop, container, false)
-
+        val shimmerRecyclerView = view.findViewById<ShimmerRecyclerView>(R.id.shimmer_recycler_view)
+        shimmerRecyclerView.showShimmer()
         return view
     }
 
