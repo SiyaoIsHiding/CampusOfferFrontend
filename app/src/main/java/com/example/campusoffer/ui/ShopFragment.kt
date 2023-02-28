423acfd8bb4defadb5b8b9077f4c42496d46a474
Fengnan <soniasun0925@gmail.com>
2023-02-27 22:47:13 -0800

    "Set up BottomSheet"

--- app/src/main/java/com/example/campusoffer/ui/ShopFragment.kt
+++ app/src/main/java/com/example/campusoffer/ui/ShopFragment.kt
@@ -1,12 +1,12 @@
 package com.example.campusoffer.ui
-
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.campusoffer.R
-//import com.example.campusoffer.databinding.FragmentShopBinding
+import com.example.campusoffer.databinding.FragmentShopBinding
 
 /**
  * Reference
@@ -16,19 +16,23 @@ import com.example.campusoffer.R
 class ShopFragment : Fragment() {
 
     // This property is only valid between onCreateView and onDestroyView.
-//    private var _binding: FragmentShopBinding? = null
-//    private val binding get() = _binding!!
+    private var _binding: FragmentShopBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-//        _binding = FragmentShopBinding.inflate(inflater, container, false)
-//        val view = binding.root
-//        _binding!!.shimmerRecyclerView.showShimmer()
-        val view = inflater.inflate(R.layout.fragment_shop, container,false)
-        return view
-    }
+        _binding = FragmentShopBinding.inflate(inflater, container, false)
+        val view = binding.root
+        _binding!!.shimmerRecyclerView.showShimmer()
 
+        binding.productsFilter.setOnClickListener {
+            findNavController().navigate(R.id.action_shopFragment_to_filterBottomSheet)
+        }
+
+            return view
+    }
 }
+
