226799ac2cb9332219f13e49ede00a2373a60fa5
Fengnan <soniasun0925@gmail.com>
2023-03-02 16:07:39 -0800

    Set up DetailPage.

--- app/src/main/java/com/example/campusoffer/ui/DetailsActivity.kt
+++ app/src/main/java/com/example/campusoffer/ui/DetailsActivity.kt
@@ -2,11 +2,55 @@ package com.example.campusoffer.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.navigation.navArgs
 import com.example.campusoffer.R
+import com.example.campusoffer.adapters.PagerAdapter
+import com.example.campusoffer.ui.fragments.ImagesFragment
+import com.example.campusoffer.ui.fragments.OverviewFragment
+import kotlinx.android.synthetic.main.activity_details.*
+import kotlinx.android.synthetic.main.placeholder_row.*
 
 class DetailsActivity : AppCompatActivity() {
+
+    private val args by navArgs<DetailsActivityArgs>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
+
+        setSupportActionBar(toolbar)
+        toolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.white))
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val fragments = ArrayList<Fragment>()
+        fragments.add(OverviewFragment())
+        fragments.add(ImagesFragment())
+
+        val titles = ArrayList<String>()
+        titles.add("Overview")
+        titles.add("More photos")
+
+        val productBundle = Bundle()
+        productBundle.putParcelable("productBundle", args.product)
+
+        val mAdapter = PagerAdapter(
+            productBundle,
+            fragments,
+            titles,
+            supportFragmentManager
+        )
+        viewPager.adapter = mAdapter
+        tabLayout.setupWithViewPager(viewPager)
+
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
---
+++ app/src/main/java/com/example/campusoffer/ui/fragments/OverviewFragment.kt
@@ -0,0 +1,33 @@
+package com.example.campusoffer.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.campusoffer.R
+import com.example.campusoffer.models.Product
+import kotlinx.android.synthetic.main.fragment_overview.view.*
+
+
+class OverviewFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_overview, container, false)
+
+        val args = arguments
+        val myBundle: Product? = args?.getParcelable("productBundle")
+
+        view.detail_title.text = myBundle?.title
+        view.detail_description.text = myBundle?.description
+
+
+
+        return view
+    }
+
+}
