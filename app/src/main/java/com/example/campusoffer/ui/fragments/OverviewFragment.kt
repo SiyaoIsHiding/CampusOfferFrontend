c878be135fa8674a7b66991cdde1de3dcfa70df6
SiyaoIsHiding <113857408+SiyaoIsHiding@users.noreply.github.com>
2023-03-08 12:01:18 -0800

    Merged from sonia

--- app/src/main/java/com/example/campusoffer/ui/fragments/OverviewFragment.kt
+++ app/src/main/java/com/example/campusoffer/ui/fragments/OverviewFragment.kt
@@ -8,24 +8,17 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
-import com.example.campusoffer.R
-import com.example.campusoffer.adapters.ImagesAdapter
-import com.example.campusoffer.adapters.ProductsAdapter
-import com.example.campusoffer.databinding.FragmentImagesBinding
 import com.example.campusoffer.databinding.FragmentOverviewBinding
 import com.example.campusoffer.models.Product
-import com.example.campusoffer.viewmodels.ImagesViewModel
+import com.example.campusoffer.models.User
 import com.example.campusoffer.viewmodels.OverviewViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import com.example.campusoffer.viewmodels.UserViewModel
 import kotlinx.android.synthetic.main.fragment_overview.view.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 @ExperimentalCoroutinesApi
 @AndroidEntryPoint
 class OverviewFragment : Fragment() {
-
-    lateinit var userViewModel: UserViewModel
 
 
     private var _binding: FragmentOverviewBinding? = null
@@ -51,6 +44,10 @@ class OverviewFragment : Fragment() {
         mView.detail_title.text = myBundle?.title
         mView.detail_price.text = myBundle?.price.toString()
         mView.detail_description.text = myBundle?.description
+        val user = User("Living in verano", "Jane", "kda98erf", "He", "siyaoh4")
+        mView.detail_seller_first_name.text = user.firstName
+        mView.detail_seller_last_name.text = user.lastName
+        mView.detail_seller_bio.text = user.bio
 
         requestCoverImage()
 
@@ -58,22 +55,13 @@ class OverviewFragment : Fragment() {
     }
 
     private fun requestCoverImage(){
-        if(myBundle._images.isNullOrEmpty()){
+        if(myBundle._images.isNullOrEmpty()) {
             Toast.makeText(
                 requireContext(),
                 "No Images Available",
                 Toast.LENGTH_SHORT
             ).show()
             return
-
-
-        view.detail_title.text = myBundle?.title
-        view.detail_price.text = myBundle?.price.toString()
-        view.detail_description.text = myBundle?.description
-        val imageBytes = myBundle?.coverImage
-        if (imageBytes != null){
-            val decodedImage = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
-            view.detail_image.setImageBitmap(decodedImage)
         }
 
         overviewViewModel.requestCoverImage(myBundle!!)
@@ -85,14 +73,6 @@ class OverviewFragment : Fragment() {
                 }
             }
         }
-        // TODO query user information via product id maybe?
-        userViewModel = UserViewModel()
-        val user = userViewModel.applyHardCodeData()
-        view.detail_seller_first_name.text = user.firstName
-        view.detail_seller_last_name.text = user.lastName
-        view.detail_seller_bio.text = user.bio
-
-        return view
     }
 
 }
